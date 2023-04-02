7012589a18b778562a125ae62bef72cc9006d330
Jayden Koh Jia Jian <66878523+jaydenkoh@users.noreply.github.com>
2023-04-02 22:01:20 +0800

    Major updates + shop order support

--- index.js
+++ index.js
@@ -13,88 +13,183 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.post("/ko-fi", async (req, res) => {
-    data = JSON.parse(req.body["data"])
+    // console.log(req.body)
+    if (req.body.constructor === Object && Object.keys(req.body).length === 0) return res.sendStatus(204)
 
-    if (data.verification_token != process.env["VERIFICATION_TOKEN"]) return;
+    let data;
 
-    const { verification_token, message_id, timestamp, type, is_public, from_name, message, amount, url, email, currency, is_subscription_payment, is_first_subscription_payment, kofi_transaction_id, tier_name, shop_items } = JSON.parse(req.body["data"]);
+    try {
+        data = JSON.parse(req.body["data"])
+    } catch (error) {
+        return res.sendStatus(400)
+    }
 
-    const date = new Date(timestamp).getTime()
+    if (data.verification_token != process.env["VERIFICATION_TOKEN"]) return res.sendStatus(403)
+
+    const date = new Date(data.timestamp).getTime()
 
     if (config.publicChannel) {
-        if (is_public == true) {
+        if (data.shop_items != null && data.shop_items.length > 0) {
             if (config.devMode) {
                 console.log(data)
             }
 
-            const embed = new MessageBuilder()
-                .setTitle('New Ko-fi Donation!')
-                .addField("Type", type, true)
-                .addField("Public?", is_public ? "Yes" : "No", true)
-                .addField("Timestamp", `<t:${date}:F>`, true)
-                .addField("Name", from_name, true)
-                .addField("Message", message != null ? message : "None", true)
-                .addField(`Amount (${currency})`, amount, true)
-                .addField("Subscription Payment?", is_subscription_payment ? "Yes" : "No", true)
-                .addField("First Subscription Payment?", is_first_subscription_payment ? "Yes" : "No", true)
-                .addField("Tier Name", tier_name == null ? "None" : tier_name, true)
-                .setColor('#00b0f4')
-                .setDescription('You just got a new donation in Ko-fi! Below are the details')
-                .setTimestamp();
+            sendEmbed(data, date, true, true, data.type).then(() => { res.sendStatus(200) })
+        } else {
+            if (data.is_public == true) {
+                if (config.devMode) {
+                    console.log(data)
+                }
 
-            hook.send(embed);
-        } else if (is_public == false) {
-            if (config.devMode) {
-                console.log(data)
+                sendEmbed(data, date, true, true).then(() => { res.sendStatus(200) })
+            } else if (data.is_public == false) {
+                if (config.devMode) {
+                    console.log(data)
+                }
+
+                sendEmbed(data, date, false, true).then(() => { res.sendStatus(200) })
             }
-
-            const embed = new MessageBuilder()
-                .setTitle('New Ko-fi Donation!')
-                .addField("Type", type, true)
-                .addField("Public?", is_public ? "Yes" : "No", true)
-                .addField("Timestamp", `<t:${date}:F>`, true)
-                .addField("Name", from_name, true)
-                .addField("Message", message != null ? "Redacted" : "None", true)
-                .addField(`Amount (${currency})`, amount, true)
-                .addField("Subscription Payment?", is_subscription_payment ? "Yes" : "No", true)
-                .addField("First Subscription Payment?", is_first_subscription_payment ? "Yes" : "No", true)
-                .addField("Tier Name", tier_name == null ? "None" : tier_name, true)
-                .setColor('#00b0f4')
-                .setDescription('You just got a new donation in Ko-fi! Below are the details')
-                .setTimestamp();
-
-            hook.send(embed);
         }
     } else {
         if (config.devMode) {
             console.log(data)
         }
 
-        const embed = new MessageBuilder()
-            .setTitle('New Ko-fi Donation!')
-            .addField('Message ID', `||${message_id}||`, true)
-            .addField("Ko-fi Transaction ID", `||${kofi_transaction_id}||`, true)
-            .addField("\u200b", "\u200b", true)
-            .addField("Type", type, true)
-            .addField("Public?", is_public ? "Yes" : "No", true)
-            .addField("Timestamp", `<t:${date}:F> (${date})`, true)
-            .addField("Name", from_name, true)
-            .addField("Message", message != null ? message : "None", true)
-            .addField(`Amount (${currency})`, amount, true)
-            .addField(`URL`, url, true)
-            .addField(`Email address`, email, true)
-            .addField("\u200b", "\u200b", true)
-            .addField("Subscription Payment?", is_subscription_payment ? "Yes" : "No", true)
-            .addField("First Subscription Payment?", is_first_subscription_payment ? "Yes" : "No", true)
-            .addField("Tier Name", tier_name == null ? "None" : tier_name, true)
-            .setColor('#00b0f4')
-            .setDescription('You just got a new donation in Ko-fi! Below are the details')
-            .setTimestamp();
-
-        hook.send(embed);
+        if (data.shop_items != null && data.shop_items.length > 0) {
+            sendEmbed(data, date, true, false, data.type).then(() => { res.sendStatus(200) })
+        } else {
+            sendEmbed(data, date, true, false).then(() => { res.sendStatus(200) })
+        }
     }
 })
 
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`)
 })
+
+// Functions
+function sendEmbed(data, date, isPublic, isPublicChannel, type) {
+    if (isPublicChannel) {
+        if (type === "Shop Order") {
+            const embed = new MessageBuilder()
+                .setTitle('New Ko-fi Shop Order!')
+                .addField("Public?", data.is_public ? "Yes" : "No", true)
+                .addField("Timestamp", `<t:${date}:F>`, true)
+                .addField("Name", data.from_name, true)
+                .addField(`Amount (${data.currency})`, data.amount, true)
+                .setColor('#00b0f4')
+                .setDescription('You just got a new shop order in Ko-fi! Below are the details')
+                .setTimestamp();
+
+            let secondMsg = ""
+
+            for (let i = 0; i < data.shop_items.length; i++) {
+                secondMsg += `__Item #${i + 1}__:\n• Direct Link Code: ${config.linkEmbed ? `https://ko-fi.com/s/${data.shop_items[i].direct_link_code}` : `<https://ko-fi.com/s/${data.shop_items[i].direct_link_code}>`}\n• Variation Name: ${data.shop_items[i].variation_name ?? "None"}\n• Quantity: ${data.shop_items[i].quantity}\n\n`
+            }
+
+            return hook.send(embed).then(() => {
+                hook.send(
+                    `=== **Purchased Shop Items** ===\n${secondMsg}`
+                )
+            })
+        } else {
+            if (isPublic) {
+                const embed = new MessageBuilder()
+                    .setTitle('New Ko-fi Donation!')
+                    .addField("Type", data.type, true)
+                    .addField("Public?", data.is_public ? "Yes" : "No", true)
+                    .addField("Timestamp", `<t:${date}:F>`, true)
+                    .addField("Name", data.from_name, true)
+                    .addField("Message", data.message ?? "None", true)
+                    .addField(`Amount (${data.currency})`, data.amount, true)
+                    .addField("Subscription Payment?", data.is_subscription_payment ? "Yes" : "No", true)
+                    .addField("First Subscription Payment?", data.is_first_subscription_payment ? "Yes" : "No", true)
+                    .addField("Tier Name", data.tier_name ?? "None", true)
+                    .setColor('#00b0f4')
+                    .setDescription('You just got a new donation in Ko-fi! Below are the details')
+                    .setTimestamp();
+
+                return hook.send(embed);
+            } else {
+                const embed = new MessageBuilder()
+                    .setTitle('New Ko-fi Donation!')
+                    .addField("Type", data.type, true)
+                    .addField("Public?", data.is_public ? "Yes" : "No", true)
+                    .addField("Timestamp", `<t:${date}:F>`, true)
+                    .addField("Name", data.from_name, true)
+                    .addField("Message", "Redacted", true)
+                    .addField(`Amount (${data.currency})`, data.amount, true)
+                    .addField("Subscription Payment?", data.is_subscription_payment ? "Yes" : "No", true)
+                    .addField("First Subscription Payment?", data.is_first_subscription_payment ? "Yes" : "No", true)
+                    .addField("Tier Name", data.tier_name ?? "None", true)
+                    .setColor('#00b0f4')
+                    .setDescription('You just got a new donation in Ko-fi! Below are the details')
+                    .setTimestamp();
+
+                return hook.send(embed);
+            }
+        }
+    } else {
+        if (type === "Shop Order") {
+            const embed = new MessageBuilder()
+                .setTitle('New Ko-fi Shop Order!')
+                .addField('Message ID', `||${data.message_id}||`, true)
+                .addField("Ko-fi Transaction ID", `||${data.kofi_transaction_id}||`, true)
+                .addField("\u200b", "\u200b", true)
+                .addField("Public?", data.is_public ? "Yes" : "No", true)
+                .addField("Timestamp", `<t:${date}:F> (${date})`, true)
+                .addField("Name", data.from_name, true)
+                .addField(`Amount (${data.currency})`, data.amount, true)
+                .addField(`URL`, data.url, true)
+                .addField(`Email address`, data.email, true)
+                .setColor('#00b0f4')
+                .setDescription('You just got a new donation in Ko-fi! Below are the details')
+                .setTimestamp();
+
+            let secondMsg = ""
+
+            for (let i = 0; i < data.shop_items.length; i++) {
+                secondMsg += `__Item #${i + 1}__:\n• Direct Link Code: ${config.linkEmbed ? `https://ko-fi.com/s/${data.shop_items[i].direct_link_code}` : `<https://ko-fi.com/s/${data.shop_items[i].direct_link_code}>`}\n• Variation Name: ${data.shop_items[i].variation_name ?? "None"}\n• Quantity: ${data.shop_items[i].quantity}\n\n`
+            }
+
+            return hook.send(embed).then(() => {
+                hook.send(
+                    `=== **Purchased Shop Items** ===\n${secondMsg}`
+                ).then(() => {
+                    if (Object.keys(data.shipping).length != 0 || data.shipping != null) {
+                        hook.send(
+                            `=== **Shipping Address** ===\nFull Name: ${data.shipping.full_name}\nStreet Address: ${data.shipping.street_address}\nCity: ${data.shipping.city}\nState/Province: ${data.shipping.state_or_province}\nPostal Code: ${data.shipping.postal_code}\nCountry: ${data.shipping.country}\nCountry Code: ${data.shipping.country_code}\nTelephone: ${data.shipping.telephone}`
+                        )
+                    } else {
+                        hook.send(
+                            `=== **No Shipping Address** ===`
+                        )
+                    }
+                })
+            })
+        } else {
+            const embed = new MessageBuilder()
+                .setTitle('New Ko-fi Donation!')
+                .addField('Message ID', `||${data.message_id}||`, true)
+                .addField("Ko-fi Transaction ID", `||${data.kofi_transaction_id}||`, true)
+                .addField("\u200b", "\u200b", true)
+                .addField("Type", data.type, true)
+                .addField("Public?", data.is_public ? "Yes" : "No", true)
+                .addField("Timestamp", `<t:${date}:F> (${date})`, true)
+                .addField("Name", data.from_name, true)
+                .addField("Message", data.message ?? "None", true)
+                .addField(`Amount (${data.currency})`, data.amount, true)
+                .addField(`URL`, data.url, true)
+                .addField(`Email address`, data.email, true)
+                .addField("\u200b", "\u200b", true)
+                .addField("Subscription Payment?", data.is_subscription_payment ? "Yes" : "No", true)
+                .addField("First Subscription Payment?", data.is_first_subscription_payment ? "Yes" : "No", true)
+                .addField("Tier Name", data.tier_name ?? "None", true)
+                .setColor('#00b0f4')
+                .setDescription('You just got a new donation in Ko-fi! Below are the details')
+                .setTimestamp();
+
+            return hook.send(embed);
+        }
+    }
+}
